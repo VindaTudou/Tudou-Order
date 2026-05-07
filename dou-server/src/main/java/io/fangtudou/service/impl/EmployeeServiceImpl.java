e0fcad89d62ba117a6e245b38a132c7cb3114742
fangtudou <1926997218@qq.com>
2026-05-07 12:06:02 +0800

    新增菜品分类管理 新增公共字段自动填充

--- dou-server/src/main/java/io/fangtudou/service/impl/EmployeeServiceImpl.java
+++ dou-server/src/main/java/io/fangtudou/service/impl/EmployeeServiceImpl.java
@@ -87,14 +87,14 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
         //设置创建时间
-        employee.setCreateTime(LocalDateTime.now());
-
-        //设置修改时间
-        employee.setUpdateTime(LocalDateTime.now());
-
-        //设置创建人id，修改人id
-        employee.setCreateUser(BaseContext.getCurrentId());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+//        employee.setCreateTime(LocalDateTime.now());
+//
+//        //设置修改时间
+//        employee.setUpdateTime(LocalDateTime.now());
+//
+//        //设置创建人id，修改人id
+//        employee.setCreateUser(BaseContext.getCurrentId());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
 
@@ -127,7 +127,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = Employee.builder()
                         .id(id)
                         .status(status)
-                        .updateTime(LocalDateTime.now()).build();
+                        .build();
+//                        .updateTime(LocalDateTime.now()).build();
         employeeMapper.update(employee);
     }
 
@@ -148,8 +149,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
 
-        employee.setUpdateTime(LocalDateTime.now());
-        employee.setUpdateUser(BaseContext.getCurrentId());
+//        employee.setUpdateTime(LocalDateTime.now());
+//        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.update(employee);
     }
